3c3a7d107abe61159ec548ba6df4569c3346589b
Cauã <cauacustodiopereira@gmail.com>
2024-09-27 10:52:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/test/coursespring/services/UserService.java
@@ -0,0 +1,25 @@
+package com.test.coursespring.services;
+
+import com.test.coursespring.entities.User;
+import com.test.coursespring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
