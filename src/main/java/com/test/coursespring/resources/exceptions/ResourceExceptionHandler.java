8ea26d0fcc46e813c1b5d0f3e35adb114d151f94
Cauã <cauacustodiopereira@gmail.com>
2024-10-01 11:34:33 -0300

    mais tratamento de erro

--- src/main/java/com/test/coursespring/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/test/coursespring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.test.coursespring.resources.exceptions;
 
+import com.test.coursespring.services.exceptions.DatabaseException;
 import com.test.coursespring.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -15,6 +16,14 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
         HttpStatus status = HttpStatus.NOT_FOUND;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
